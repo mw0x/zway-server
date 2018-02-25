5a2caffdfdac3cb71b2c1c05eb72f800f5a84e92
Marc <marc@atomicode.de>
2018-02-25 19:59:02 +0100

    file headers updated

--- include/fcmsender.h
+++ include/fcmsender.h
@@ -1,10 +1,40 @@
 
+// ============================================================ //
+//
+//   d88888D db   d8b   db  .d8b.  db    db
+//   YP  d8' 88   I8I   88 d8' `8b `8b  d8'
+//      d8'  88   I8I   88 88ooo88  `8bd8'
+//     d8'   Y8   I8I   88 88~~~88    88
+//    d8' db `8b d8'8b d8' 88   88    88
+//   d88888P  `8b8' `8d8'  YP   YP    YP
+//
+//   open-source, cross-platform, crypto-messenger
+//
+//   Copyright (C) 2018 Marc Weiler
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// ============================================================ //
+
 #ifndef SERVER_FCM_SENDER_H_
 #define SERVER_FCM_SENDER_H_
 
 #include <curl/curl.h>
 
 #include <string>
+
+// ============================================================ //
 
 class FcmSender
 {
@@ -34,4 +64,7 @@ public:
 
 };
 
+// ============================================================ //
+
 #endif
+
--- src/fcmsender.cpp
+++ src/fcmsender.cpp
@@ -1,4 +1,32 @@
 
+// ============================================================ //
+//
+//   d88888D db   d8b   db  .d8b.  db    db
+//   YP  d8' 88   I8I   88 d8' `8b `8b  d8'
+//      d8'  88   I8I   88 88ooo88  `8bd8'
+//     d8'   Y8   I8I   88 88~~~88    88
+//    d8' db `8b d8'8b d8' 88   88    88
+//   d88888P  `8b8' `8d8'  YP   YP    YP
+//
+//   open-source, cross-platform, crypto-messenger
+//
+//   Copyright (C) 2018 Marc Weiler
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// ============================================================ //
+
 #include "fcmsender.h"
 #include "logger.h"
 
@@ -6,6 +34,8 @@
 
 const char *FcmSender::fcmUrl = "https://fcm.googleapis.com/fcm/send";
 const char *FcmSender::fcmServerKey = "AAAAPYes6ds:APA91bEy1dPjayq9vOzI-2pY2JB3FmtS6_wc8B1NT_LhJPjND_eBpnWK3u8zmcXEfGfC-eies-9WMTnRiOgDpfuNnWh-aHaqMeGwrNuGB92iFd2fchSP0Yselew1ZY3xjmcLvvsnWaIx";
+
+// ============================================================ //
 
 bool FcmSender::startup()
 {
@@ -94,3 +124,6 @@ size_t FcmSender::writeCallback(void *ptr, size_t size, size_t nmemb, void *user
 {
     return size * nmemb;
 }
+
+// ============================================================ //
+
